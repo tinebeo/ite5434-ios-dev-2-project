5e5691491958d5ab4440f0d8abb46b8aa68fd4b7
Christine Ebeo <caebeo@hotmail.com>
2022-03-18 13:33:08 -0400

    CE - added RentToGo using swiftui

---
+++ RentToGo/RentToGo/RentToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentToGoApp.swift
+//  RentToGo
+//
+//  Created by Christine Ebeo on 2022-03-18.
+//
+
+import SwiftUI
+
+@main
+struct RentToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
