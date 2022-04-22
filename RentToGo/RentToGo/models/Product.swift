93689e35869c6cd4102da0f16912a041d1994cf0
Christine Ebeo <caebeo@hotmail.com>
2022-04-21 22:38:36 -0400

    ce - updated product, need to add those items that are commented out

--- RentToGo/RentToGo/models/Product.swift
+++ RentToGo/RentToGo/models/Product.swift
@@ -9,30 +9,39 @@ import Foundation
 
 struct Product : Identifiable, Hashable  {
     var id = UUID().uuidString
-
     var name : String
     var price : Double
     var description : String
     var quantity : Int
     var isReturned : Bool
+    //var owner : String?
+    //var category : String?
+    
     
     init(_ name: String, _ price: Double, _ description: String, _ quantity: Int, _ isReturned: Bool) {
+        //_ owner : String?, _ category : String?
         self.name = name
         self.price = price
         self.description = description
         self.quantity = quantity
         self.isReturned = isReturned
+        //self.owner = owner
+        //self.category = category
         
     }
     
     func getDictionary() -> Dictionary<String, Any> {
         
         var dict = [
+            "id" : id,
             "name": name,
             "price": price,
             "description": description,
             "quantity": quantity,
             "isReturned": false
+            //"owner": owner,
+            //"category" : category
+            
         ] as [String : Any]
         
         return dict
