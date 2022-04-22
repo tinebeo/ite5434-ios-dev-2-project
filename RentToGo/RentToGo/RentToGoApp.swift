4da25eb6fec906055b98d212b3675505d9f1bc35
Tim Baklanov <timbaklanov03@gmail.com>
2022-04-21 23:00:13 -0400

    added productdao

--- RentToGo/RentToGo/RentToGoApp.swift
+++ RentToGo/RentToGo/RentToGoApp.swift
@@ -7,11 +7,13 @@
 
 import SwiftUI
 import Firebase
+import Stripe
 
 @main
 struct RentToGoApp: App {
     
     init(){
+        StripeAPI.defaultPublishableKey = "pk_test_51KeOQoLRE6j3B7vaxcTYB870BBGSPy7K6YeAZ4jhXnJl9w8D6VPMRcPRyDshfNOhQhrTNxbpss1wLh4abDUZlIFV00L8Dlk5fP"
         initFirebase()
         ProductDao()
     }
--- RentToGo/RentToGo/dao/ProductDao.swift
+++ RentToGo/RentToGo/dao/ProductDao.swift
@@ -2,6 +2,12 @@
 //  ProductDao.swift
 //  RentToGo
 //
+//  Created by Tim Baklanov on 2022-04-21.
+//
+//
+//  ProductDao.swift
+//  RentToGo
+//
 //  Created by Christine Ebeo on 2022-04-21.
 //
 
